e8bed6a1b15f769573b8101c7603200f0c9ac172
Chuba_TGC <rshtirme@ramapo.edu>
2019-12-08 20:43:20 -0500

    push references

--- src/sirt6/pages/func.py
+++ src/sirt6/pages/func.py
@@ -5,7 +5,10 @@ from dash.dependencies import Input, State, Output
 
 from ..app import app
 
-markdown = dcc.Markdown('''
+markdown = markdown = dcc.Markdown(
+    dangerously_allow_html=True,
+        children=[
+'''
 # Major Functions of SIRT6
 ___
  1. ADP-Ribosyl Transferase
@@ -14,53 +17,53 @@ ___
  4. Telomeric Chromatin Maintenance
  5. Inflammation
  6. Lipid Metabolism
- 7. Glucose Metabolism
+ 7. Glucose Metabolism[<sup>25</sup>](/references#25)
 
 ## DNA Repair
 ___
-Mammalian SIRT6 protein plays an important role in DNA repair and cells overall genomic stability.  It deals with both DNA double stranded breaks as well as base excision repairs. In mammalian cells under oxidative stress, it was shown that SIRT6 is recruited to DNA double strand breaks and starts the repair process.  This is through the use of both non-homologous end joining and homologous recombination. SIRT6 associates with PARP1, starting poly-ADP-ribosylase activity and improving DNA repair while under oxidative stress. SIRT6 is also associated with chromatin. Cells that have low levels of chromatin display higher levels of genomic instability, linked to both aging and tumor suppression in mammals.
+Mammalian SIRT6 protein plays an important role in DNA repair and cells overall genomic stability.  It deals with both DNA double stranded breaks as well as base excision repairs. In mammalian cells under oxidative stress, it was shown that SIRT6 is recruited to DNA double strand breaks and starts the repair process.  This is through the use of both non-homologous end joining and homologous recombination. SIRT6 associates with PARP1, starting poly-ADP-ribosylase activity and improving DNA repair while under oxidative stress. SIRT6 is also associated with chromatin. Cells that have low levels of chromatin display higher levels of genomic instability, linked to both aging and tumor suppression in mammals.[<sup>24</sup>](/references#24) [<sup>14</sup>](/references#14)
 
 ![Figure thumbnail fx1](https://els-jbs-prod-cdn.literatumonline.com/cms/attachment/c82b9faf-484b-4fc7-be6a-3318f709565e/fx1.jpg)
 Image Source: [https://www.cell.com/molecular-cell/fulltext/S1097-2765(13)00475-9](https://www.cell.com/molecular-cell/fulltext/S1097-2765(13)00475-9)
 
-The above diagram shows SIRT6 as one of the first factors recruited to a double-strand break in a DNA molecule.  SIRT6 then brings SNF2H, a chromatin remodeler, which deacetylates histone H3K56.  This chromatin remodeling at the respective sites of damage help prevent the recruitment of downstream factors, including RPA, BRCA1, and S3BP1.
+The above diagram shows SIRT6 as one of the first factors recruited to a double-strand break in a DNA molecule.  SIRT6 then brings SNF2H, a chromatin remodeler, which deacetylates histone H3K56.  This chromatin remodeling at the respective sites of damage help prevent the recruitment of downstream factors, including RPA, BRCA1, and S3BP1.[<sup>24</sup>](/references#24)
 
 
 ## Histone Deacetylase Activities
 ___
-Human SIRT6 is an NAD+ dependent, histone H3 lysine 9 (H3K9) deacetylase that regulates telomeric chromatin. More specifically, SIRT6 deals with end-to-end chromosomal fusion as well as cellular senescence. In SIRT6 depleted cells, telomere structures appear abnormal. Further research has also showed SIRT6 to directly decrease expression of multiple glucose-metabolic genes through the deacetylation of H3K9 at their promoters.
+Human SIRT6 is an NAD+ dependent, histone H3 lysine 9 (H3K9) deacetylase that regulates telomeric chromatin. More specifically, SIRT6 deals with end-to-end chromosomal fusion as well as cellular senescence. In SIRT6 depleted cells, telomere structures appear abnormal. Further research has also showed SIRT6 to directly decrease expression of multiple glucose-metabolic genes through the deacetylation of H3K9 at their promoters.[<sup>13</sup>](/references#13)
 
 ![Figure thumbnail fx1](https://els-jbs-prod-cdn.literatumonline.com/cms/attachment/1f7d3963-d43c-44da-ad76-a10af4419c31/fx1.jpg)
 Image Source: [https://www.cell.com/cell-reports/fulltext/S2211-1247(17)30325-X](https://www.cell.com/cell-reports/fulltext/S2211-1247(17)30325-X)
 
-The diagram above shows SIRT6 as a histone deacetylase that promotes DNA repair.  SIRT6 helps regulate Tau protein stability and phosphorylation by increasing the activation of kinase GSK3α/β.
+The diagram above shows SIRT6 as a histone deacetylase that promotes DNA repair.  SIRT6 helps regulate Tau protein stability and phosphorylation by increasing the activation of kinase GSK3α/β.[<sup>10</sup>](/references#10)
 
 # Major Biological Pathways
 ___
- 1. Removal of long-chain fatty acyl groups from lysine residues
+ 1. Removal of long-chain fatty acyl groups from lysine residues[<sup>9</sup>](/references#9)
  2. DNA Repair
  3. Glycolysis
  4. Gluconeogenesis
  5. Tumorigenesis
  6. Neurodegeneration
- 7. Cardiac Hypertrophic Responses
+ 7. Cardiac Hypertrophic Responses[<sup>11</sup>](/references#11)
  8. Gene Expression
  9. Apoptosis
- 10. Cellular Senescence
+ 10. Cellular Senescence[<sup>31</sup>](/references#31)
 
 ## Glycolysis
 ___
 ![](https://ars.els-cdn.com/content/image/1-s2.0-S0092867409016274-fx1.jpg)
 Image Source: [https://www.sciencedirect.com/science/article/pii/S0092867409016274](https://www.sciencedirect.com/science/article/pii/S0092867409016274)
 
-As previously stated, SIRT6 functions as a histone H3K9 deacetylase.  This controls the expression of many glycolytic genes. It is noted that SIRT6 appears to function as a corepressor of the transcription factor Hif1α.  This transcription factor is a very important regulator of the nutrient stress responses.  When there is lower amounts of SIRT6, Hif1α activity is increased.  This leads to higher levels of glucose uptake with up-regulation of glycolysis and lower mitochondrial respiration. Studies have also shown that lack of SIRT6 led to an increase in serum lactate, further supporting its role in glucose uptake regulation and a glycolytic switch.
+As previously stated, SIRT6 functions as a histone H3K9 deacetylase.  This controls the expression of many glycolytic genes. It is noted that SIRT6 appears to function as a corepressor of the transcription factor Hif1α.  This transcription factor is a very important regulator of the nutrient stress responses.  When there is lower amounts of SIRT6, Hif1α activity is increased.  This leads to higher levels of glucose uptake with up-regulation of glycolysis and lower mitochondrial respiration. Studies have also shown that lack of SIRT6 led to an increase in serum lactate, further supporting its role in glucose uptake regulation and a glycolytic switch.[<sup>34</sup>](/references#34)
 
 ## Tumorigenesis
 ___
 ![](https://ars.els-cdn.com/content/image/1-s2.0-S0092867412013517-fx1.jpg)
 Image Source: [https://www.sciencedirect.com/science/article/pii/S0092867412013517](https://www.sciencedirect.com/science/article/pii/S0092867412013517)
 
-SIRT6 has been identified as a tumor suppressor, regulating aerobic glycolysis in many cancer cells.  Research suggests that SIRT6 plays a role in the establishment and maintenance of cancer.  This is due to tumor formation without activation of any known oncogenes associated with a loss of SIRT6.  Transformed SIRT6 deficient cells also displayed both increased glycolysis and tumor growth, with studies in vivo showing SIRT6 deletion leads to increased number, size, and aggressiveness of tumors.  It also functions as a regulator of ribosome metabolism and is selectively suppressed in many human cancers, showing its importance in reducing cancer metabolism.
+SIRT6 has been identified as a tumor suppressor, regulating aerobic glycolysis in many cancer cells.  Research suggests that SIRT6 plays a role in the establishment and maintenance of cancer.  This is due to tumor formation without activation of any known oncogenes associated with a loss of SIRT6.  Transformed SIRT6 deficient cells also displayed both increased glycolysis and tumor growth, with studies in vivo showing SIRT6 deletion leads to increased number, size, and aggressiveness of tumors.  It also functions as a regulator of ribosome metabolism and is selectively suppressed in many human cancers, showing its importance in reducing cancer metabolism.[<sup>21</sup>](/references#21)
 
 
 # Major Tissue Expression
@@ -77,14 +80,15 @@ ___
  9. Placenta
  10. Heart
  11. Testes
- 12. Ovaries
+ 12. Ovaries[<sup>2</sup>](/references#2)
+
 ### Isoform L
  1. Lung
  2. Chondrosarcoma
  3. Colon
  4. Cell Lines
  5. Duodenum
- 6. Prostate
+ 6. Prostate[<sup>2</sup>](/references#2)
 
 # Developmental Stages
 
@@ -95,15 +99,15 @@ ___
  5. Intestine
  6. Kidney
  7. Lung
- 8. Stomach
+ 8. Stomach[<sup>2</sup>](/references#2) [<sup>25</sup>](/references#25)
 
 # Sub-Cellular Localization
 ___
-SIRT6 is a predominantly nuclear protein. This is associated mostly with telomeric heterochromatin regions, having a strong association with DNA repair and regulation of telomeric chromatin.
+SIRT6 is a predominantly nuclear protein. This is associated mostly with telomeric heterochromatin regions, having a strong association with DNA repair and regulation of telomeric chromatin.[<sup>30</sup>](/references#30)
 
 # Protein Interactions
 ___
-SIRT6 has many protein interactions, functioning in DNA repair, cell cycle regulation, and NF-kB transcription.  The top 5 proteins that SIRT6 is known to interact with include: CHD3, MYC, RELA, AHR, and AKT1
+SIRT6 has many protein interactions, functioning in DNA repair, cell cycle regulation, and NF-kB transcription.  The top 5 proteins that SIRT6 is known to interact with include: CHD3, MYC, RELA, AHR, and AKT1[<sup>5</sup>](/references#5)
 
 CHD3: This protein is a component of a histone deacetylase complex known as the Mi-2/NuRD complex, which participates in the remodeling of chromatin by deacetylating histones.  This process is important for many processes including transcription.
 
@@ -113,8 +117,8 @@ RELA: This protein binds with NF-kappa-B to form a heterodimeric complex.  This 
 
 AHR: This protein is a transcription factor involved in the regulation of biological responses to halogenated aromatic hydrocarbons by mediating biochemical and toxic effects of them.
 
-AKT1: This protein is a serine/threonine-protein kinase responsible for regulating many processes.  This includes metabolism, proliferation, cell survival, growth and angiogenesis.
-''')
+AKT1: This protein is a serine/threonine-protein kinase responsible for regulating many processes.  This includes metabolism, proliferation, cell survival, growth and angiogenesis.[<sup>5</sup>](/references#5)
+'''])
 
 
 
--- src/sirt6/pages/struct.py
+++ src/sirt6/pages/struct.py
@@ -9,24 +9,30 @@ markdown1 = dcc.Markdown('''
 # 3D Model of SIRT6
 ''')
 
-markdown2 = dcc.Markdown('''
-The protein shown to the left represents the SIRT6 protein isoform 1 structure.  This protein consists of a 355 amino acid structure, containing 13 alpha helices with 87 residues and 14 beta strands with 60 residues.  It has an active site at position 133 with four zinc metal binding sites at 141, 144, 166, and 177.  It also has a NAD binding site at position 258.
-
-SIRT6 protein is shown to have a Quaternary structure with association to the RELA protein. While another research article by Kaidi et al. (2010) showed potential relation to RBBP8 associated with its quaternary structure, this information was retracted due to falsified data and will not be discussed.
+markdown2 = markdown = dcc.Markdown(
+    dangerously_allow_html=True,
+        children=[
 '''
+The protein shown above represents the SIRT6 protein isoform 1 structure.  This protein consists of a 355 amino acid structure, containing 13 alpha helices with 87 residues and 14 beta strands with 60 residues.  It has an active site at position 133 with four zinc metal binding sites at 141, 144, 166, and 177.  It also has a NAD binding site at position 258.[<sup>30</sup>](/references#30)
+
+SIRT6 protein is shown to have a Quaternary structure with association to the RELA protein.[<sup>31</sup>](/references#31) While another research article by Kaidi et al. (2010) showed potential relation to RBBP8 associated with its quaternary structure, this information was retracted due to falsified data and will not be discussed.[<sup>30</sup>](/references#30) [<sup>18</sup>](/references#18)
+''']
 )
 
 markdown3 = dcc.Markdown('''
 # Functional Domain
 ''')
 
-markdown4 = dcc.Markdown('''
-According to information found on UniProt, SIRT6 has one main deacetylase sirtuin-type domain. This is a 240 amino acid sequence at position 35-274.  When in complex with NAD, this domain consists of a Rossmann fold and a small domain containing a three-stranded zinc ribbon motif.
+markdown4 = markdown = dcc.Markdown(
+    dangerously_allow_html=True,
+        children=[
+'''
+According to information found on UniProt, SIRT6 has one main deacetylase sirtuin-type domain. This is a 240 amino acid sequence at position 35-274.[<sup>30</sup>](/references#30)  When in complex with NAD, this domain consists of a Rossmann fold and a small domain containing a three-stranded zinc ribbon motif.[<sup>27</sup>](/references#27)
 
-This deacetylase sirtuin-type domain falls under the SIR2 superfamily of proteins also classified as sirtuins.  This includes silent information regulator 2 (Sir2) enzymes responsible for catalyzing NAD+ dependent protein and histone deacetylation, where the acetyl group from the lysine epsilon-amino group is transferred to the ADP-ribose within the NAD+. This produces nicotinamide and a novel metabolite O-acetyl-ADP-ribose.
+This deacetylase sirtuin-type domain falls under the SIR2 superfamily of proteins also classified as sirtuins.  This includes silent information regulator 2 (Sir2) enzymes responsible for catalyzing NAD+ dependent protein and histone deacetylation, where the acetyl group from the lysine epsilon-amino group is transferred to the ADP-ribose within the NAD+. This produces nicotinamide and a novel metabolite O-acetyl-ADP-ribose.[<sup>27</sup>](/references#27) [<sup>36</sup>](/references#36)
 
-Sirtuins are found in all eukaryotes as well as many archaea and prokaryotes.  These proteins have been shown to be associated with regulating gene silencing, DNA repair, metabolic enzymes, and life span.
-''')
+Sirtuins are found in all eukaryotes as well as many archaea and prokaryotes.  These proteins have been shown to be associated with regulating gene silencing, DNA repair, metabolic enzymes, and life span.[<sup>36</sup>](/references#36)
+'''])
 
 iFrame = html.Iframe(src="https://www.ncbi.nlm.nih.gov/Structure/icn3d/full.html?mmdbid=163934&width=500&height=500&showcommand=0&mobilemenu=1&showtitle=1",
             height=400,
